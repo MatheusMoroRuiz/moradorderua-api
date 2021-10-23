d56f165626cb5323ec993700d9bee5b26d5de97a
MATHEUS MORO RUIZ <matheus.mruiz2@senacsp.edu.br>
2021-10-22 22:24:30 -0300

    Arquivos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 var express = require("express");
 var morgan = require("morgan");
+var cors = require("cors");
 const { database } = require("./models");
 var app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use(morgan("combined"));
 
@@ -12,10 +14,10 @@ var tipoUsuario = require("./routes/tipoUsuario");
 var telefone = require("./routes/telefone");
 var ong = require("./routes/ongs");
 
-app.use("/usuario", usuarios);
-app.use("/tipousuario", tipoUsuario);
-app.use("/telefone", telefone);
-app.use("/ong", ong);
+app.use("/usuarios", usuarios);
+app.use("/usuario_tipos", tipoUsuario);
+app.use("/telefones", telefone);
+app.use("/ongs", ong);
 
 app.get("/", function (req, res) {
   res.send("MORADOR DE RUA - API");
